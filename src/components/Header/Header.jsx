612777a1485c0cbbb1564a635f9d3221f5bbed30
root <alaeddine.beljebbar@gmail.com>
2018-12-28 01:09:39 +0100

    hour style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
+const setTime = () => {
+  const m = moment();
+  const second = m.format('ss');
+  const minute = m.format('mm');
+  const hour = m.format('hh');
+  return {
+    hour,
+    minute,
+    second
+  };
+}
+
 const Header = () => {
   const toDay = moment().format('dddd D MMMM');
-  const timeNow = moment().format('HH:mm:ss');
-  const [ time, tick ] = useState(timeNow);
+  const [ time, tick ] = useState(setTime());
   const [ day, getDay ] = useState(toDay);
   useEffect(() => {
     const intervalID = setInterval(
-      tick(moment().format('HH:mm:ss')), 1000
-    )
+      () => tick(setTime()), 1000
+    );
     return () => {
       clearInterval(intervalID);
     }
-  });
-  useEffect(() => getDay(moment().format('dddd D MMMM')))
+  }, [time]);
+  useEffect(() => getDay(moment().format('dddd D MMMM')), []);
+  const { hour, minute, second } = time;
   return(
     <div className="columns is-mobile is-gapless is-vcentered">
       <div className="column is-6">
         <p className="dateStyle">{ day }</p>
       </div>
       <div className="column is-6">
-        <p className="timeStyle">{ time }</p>
+        <p className="timeStyle">
+          {`${hour} : ${minute} `}<sup className="secondStyle">{second}</sup>
+        </p>
       </div>
     </div>
   )
